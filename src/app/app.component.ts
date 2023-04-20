ec3a60fb6ac0202d21fc2bf62788d6fb9e9a33dd
adovgiy <adovgiy@zaoblako.ru>
2023-04-20 13:27:03 +0300

    Основная верстка

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { InputNumberModule } from 'primeng/inputnumber';
+import { SplitterModule } from 'primeng/splitter';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [CommonModule, InputNumberModule, FormsModule],
+  imports: [CommonModule, SplitterModule],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  public val = 0;
-}
+export class AppComponent {}
