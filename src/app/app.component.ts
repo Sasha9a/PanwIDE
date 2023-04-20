9746ba871185695d5bba0b2160f9606e3830aac8
adovgiy <adovgiy@zaoblako.ru>
2023-04-20 13:06:54 +0300

    Добавил PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [CommonModule],
+  imports: [CommonModule, InputNumberModule, FormsModule],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  public title = 'pawn-ide';
+  public val = 0;
 }
