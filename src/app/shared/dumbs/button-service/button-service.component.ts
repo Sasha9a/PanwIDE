ad0fffcd50b996bbac9619764c88a25621934b73
adovgiy <adovgiy@zaoblako.ru>
2023-05-04 10:19:39 +0300

    Список сервисов в боковой панели

--- src/app/shared/dumbs/button-service/button-service.component.ts
+++ src/app/shared/dumbs/button-service/button-service.component.ts
@@ -1,9 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { DragDropModule } from 'primeng/dragdrop';
 import { TooltipModule } from 'primeng/tooltip';
+import { Observable } from 'rxjs';
+import { PanelEnum } from '../../../core/enums/panel.enum';
 import { ServiceTypeEnum } from '../../../core/enums/service.type.enum';
+import { GlobalPanelInterface } from '../../../core/interfaces/global.storage.interface';
 import { GlobalDragButtonService } from '../../../core/services/global-drag-button.service';
+import { GlobalStorageService } from '../../../core/services/global.storage.service';
 
 @Component({
   selector: 'app-button-service',
@@ -12,8 +16,11 @@ import { GlobalDragButtonService } from '../../../core/services/global-drag-butt
   templateUrl: './button-service.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ButtonServiceComponent {
+export class ButtonServiceComponent implements OnInit {
   @Input() public serviceType: ServiceTypeEnum;
+  @Input() public panelType: PanelEnum;
+
+  public panelInfo$: Observable<GlobalPanelInterface>;
 
   public readonly serviceInfo: Record<ServiceTypeEnum, { src: string; tooltip: string }> = {
     [ServiceTypeEnum.PROJECT]: {
@@ -22,7 +29,15 @@ export class ButtonServiceComponent {
     }
   };
 
-  public constructor(private readonly globalDragButtonService: GlobalDragButtonService) {}
+  public constructor(
+    private readonly globalDragButtonService: GlobalDragButtonService,
+    private readonly globalStorageService: GlobalStorageService
+  ) {}
+
+  public ngOnInit() {
+    const keyPanel = this.globalStorageService.convertPanelTypeToKey(this.panelType);
+    this.panelInfo$ = this.globalStorageService.select((state) => state[keyPanel]);
+  }
 
   public dragStart() {
     this.globalDragButtonService.setState(this.serviceType);
