70c63e50bc03254a8987c64d2f2b1a0475e1047f
adovgiy <adovgiy@zaoblako.ru>
2023-05-19 10:15:42 +0300

    Исправление ошибки с перетаксиванием файлов

--- src/app/shared/pipes/is-droppable-project-file.pipe.ts
+++ src/app/shared/pipes/is-droppable-project-file.pipe.ts
@@ -1,14 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { ServiceProjectItemInterface } from '../../../../libs/interfaces/service.project.item.interface';
-import { ElectronService } from '../../core/services/electron.service';
 
 @Pipe({
   name: 'isDroppableProjectFile',
   standalone: true
 })
 export class IsDroppableProjectFilePipe implements PipeTransform {
-  public constructor(private readonly electronService: ElectronService) {}
-
   public transform(selectedItems: ServiceProjectItemInterface[], item: ServiceProjectItemInterface): boolean {
     if (!item.isDirectory) {
       return false;
@@ -18,12 +15,7 @@ export class IsDroppableProjectFilePipe implements PipeTransform {
       return false;
     }
 
-    if (
-      selectedItems.every(
-        (selectedItem) =>
-          item.fullPath === selectedItem.fullPath.slice(0, selectedItem.fullPath.lastIndexOf(this.electronService.isWin ? '\\' : '/'))
-      )
-    ) {
+    if (item.children?.some((childItem) => selectedItems?.some((selectedItem) => selectedItem.name === childItem.name))) {
       return false;
     }
 
