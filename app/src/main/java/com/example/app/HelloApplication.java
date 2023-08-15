e4cad0af9098d4dbba4ca6cca5d28b1e836c92c1
Aleksandr <aleksandr_dovgiyy@mail.ru>
2023-08-15 23:28:57 +0300

    Настройка Java and Angular

--- app/src/main/java/com/example/app/HelloApplication.java
+++ app/src/main/java/com/example/app/HelloApplication.java
@@ -1,26 +1,16 @@
 package com.example.app;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.util.Objects;
-
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+    public void start(Stage stage) {
         stage.setTitle("Demo");
         WebView browser = new WebView();
-        WebEngine webEngine = browser.getEngine();
-        ClassLoader classLoader = getClass().getClassLoader();
-        String s = Objects.requireNonNull(classLoader.getResource("index.html")).toExternalForm();
-        System.out.println(s);
-        webEngine.load(s);
+        browser.getEngine().load(getClass().getResource("index.html").toString());
         Scene scene = new Scene(browser);
         stage.setScene(scene);
 
